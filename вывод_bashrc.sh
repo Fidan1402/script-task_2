8d725726a21e17aea898697875d308c928e6ca12
Фидан <fidan1402@mail.ru>
2025-03-15 21:13:02 +0300

    Ваше описание изменений

--- вывод_bashrc.sh
+++ вывод_bashrc.sh
@@ -5,4 +5,4 @@
    # Получаем строки с 12 по 45 из ~/.bashrc и сохраняем в output.txt
    sed -n '12,45p' ~/.bashrc > output.txt
 
-   echo "Строки с 12 по 45 из ~/.bashrc сохранены в $output_file"
+   echo "Строки с 12 по 45 из ~/.bashrc сохранены в output.txt"
